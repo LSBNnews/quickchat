3a4c4c59b3dde7b45db4d8ee81d22c36d8d7d2df
DzL <dinzilo666@gmail.com>
2025-01-25 10:07:28 +0700

    remove unessential library

--- app/src/main/java/com/example/quickchat/MainActivity.java
+++ app/src/main/java/com/example/quickchat/MainActivity.java
@@ -4,11 +4,7 @@ import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 import android.content.Intent;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
@@ -20,11 +16,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
         // Tìm button Signup trong file XML
         Button signUpButton = findViewById(R.id.signUpButton);
         Button logInButton = findViewById(R.id.SignInButton);
