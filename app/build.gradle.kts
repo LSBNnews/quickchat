3c929b38ad509c394c433e9bd02129b737a48c23
DzL <dinzilo666@gmail.com>
2025-01-26 12:13:27 +0700

    fix gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,64 +1,21 @@
-plugins {
-    alias(libs.plugins.android.application)
-
-    id("com.google.gms.google-services")
-}
-
-android {
-    namespace = "com.example.quickchat"
-    compileSdk = 35
-
-    defaultConfig {
-        applicationId = "com.example.quickchat"
-        minSdk = 35
-        targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-    buildFeatures {
-        viewBinding = true
-    }
-}
-
 dependencies {
-    implementation(platform("com.google.firebase:firebase-bom:33.8.0"))
-    implementation("com.google.firebase:firebase-analytics")
+    implementation(platform(libs.google.firebase.bom))
 
-    // Import the BoM for the Firebase platform
-    implementation(platform("com.google.firebase:firebase-bom:33.8.0"))
-
-    // Add the dependency for the Firebase Authentication library
-    implementation("com.google.firebase:firebase-auth")
-    implementation("com.google.firebase:firebase-analytics:22.2.0")
+    // Thư viện Firebase Authentication
+    implementation(libs.google.firebase.auth)
+    implementation(libs.firebase.database)
 
     // Authentication with Credential Manager
-    implementation("com.google.android.gms:play-services-auth:21.3.0")
+    implementation(libs.play.services.auth)
 
     // UI homeScreen
-    implementation("de.hdodenhof:circleimageview:2.2.0")
-    implementation("com.github.bumptech.glide:glide:4.8.0")
+    implementation(libs.circleimageview)
+    implementation(libs.glide)
 
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
-    implementation(libs.firebase.database)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
